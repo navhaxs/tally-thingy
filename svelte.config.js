1dcdf09ab1c45da62fefa08ca8d1991b10a1f222
Jeremy Wong <navhaxs@hotmail.com>
2025-08-10 19:45:09 +1000

    add strict: false workaround

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/tally-thingy' : ''
-		}
-	}
+    kit: {
+		adapter: adapter({ strict: false }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/tally-thingy' : ''
+        },
+        prerender: {
+            entries: ['*']
+        }
+    }
 };
 
 export default config;
